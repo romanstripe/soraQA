701f07c51bf1ce43048c45062682525f9dca8c04
Sey <ppresi14789@gmail.com>
2026-04-07 12:45:05 +0900

    feat: add vedio upload process in Landing page

---
+++ tests/auth.spec.ts
@@ -0,0 +1,27 @@
+//auth.spec.ts
+
+import { test, expect } from "@playwright/test";
+import { LandingPage } from "../models/LandingPage";
+import { LoginPage } from "../models/LoginPage";
+import { KakaoLoginPage } from "../models/KakaoLoginPage";
+
+test.describe("Login test", () => {
+  test.beforeEach("Go to Login Page", async ({ page }) => {
+    const landingPage = new LandingPage(page);
+    await landingPage.goto(); //1. 접속
+    await landingPage.clickLogin(); //2. 로그인 버튼 클릭
+    await expect(page).toHaveURL(/.*login/); //3. 로그인 페이지로 이동했는지 확인
+  });
+
+  test("Id/Pw login", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.login();
+    await expect(page).toHaveURL(/.*service/);
+  });
+
+  test("Kakao login", async ({ page }) => {
+    const kakaoLoginPage = new KakaoLoginPage(page);
+    await kakaoLoginPage.kakaoLogin();
+    await expect(page).toHaveURL(/.*service/);
+  });
+});
--- tests/service.spec.ts
+++ tests/service.spec.ts
@@ -1,7 +1,8 @@
-// tests/sora.spec.ts
+// tests/service.spec.ts
 
 import { test, expect } from "@playwright/test";
 import { LandingPage } from "../models/LandingPage";
+import { LoginPage } from "../models/LoginPage";
 
 test("Video URL input test", async ({ page }) => {
   const landingPage = new LandingPage(page);
